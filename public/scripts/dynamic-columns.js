60848505a2c5e944e536c70390bcfc5b6d352349
Joh Yoshida <johyoshida@gmail.com>
2018-01-13 15:43:06 -0800

    Removed alerts

--- public/scripts/dynamic-columns.js
+++ public/scripts/dynamic-columns.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $(window).on('resize', function() {
     var win= $(this);
     if (win.width() > 1025) {
-      alert('desktop');
       $('programming-languages').addClass('three columns');
       $('web-dev').addClass('six columns');
       $('game-dev').addClass('three columns');
@@ -10,7 +9,6 @@ $(document).ready(function() {
       $('web-dev').removeClass('one column');
       $('game-dev').removeClass('one column');
     } else {
-      alert('mobile');
       $('programming-languages').addClass('one column');
       $('web-dev').addClass('one column');
       $('game-dev').addClass('one column');
