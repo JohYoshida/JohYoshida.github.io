0d74c6e73dd610273a0bbaeb7c821b3ea4d89de2
Joh Yoshida <johyoshida@gmail.com>
2018-01-13 15:25:29 -0800

    Dynamic skills columns

---
+++ public/scripts/dynamic-columns.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $(window).on('resize', function() {
+    if (win.width() > 1025) {
+      alert('desktop');
+      $('programming-languages').addClass('three columns');
+      $('web-dev').addClass('six columns');
+      $('game-dev').addClass('three columns');
+      $('programming-languages').removeClass('one column');
+      $('web-dev').removeClass('one column');
+      $('game-dev').removeClass('one column');
+    } else {
+      alert('mobile');
+      $('programming-languages').addClass('one column');
+      $('web-dev').addClass('one column');
+      $('game-dev').addClass('one column');
+      $('programming-languages').removeClass('three columns');
+      $('web-dev').removeClass('six columns');
+      $('game-dev').removeClass('three columns');
+    }
+  });
+});
