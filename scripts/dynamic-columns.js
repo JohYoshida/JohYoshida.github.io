b68b42dd936ec5fa075e538f162df8ba193f20ea
Joh Yoshida <johyoshida@gmail.com>
2018-01-13 16:50:10 -0800

    Do on page load instead of window resize

--- scripts/dynamic-columns.js
+++ scripts/dynamic-columns.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
+  var win= $(this);
+  if (win.width() > 750) {
+    $('.programming-languages').addClass('three columns');
+    $('.web-dev').addClass('six columns');
+    $('.game-dev').addClass('three columns');
+    $('.edu-program').addClass('six columns');
+    $('.employer').addClass('six columns');
+    $('.project').addClass('six columns');
+  } else {
+    $('.programming-languages').removeClass('three columns');
+    $('.web-dev').removeClass('six columns');
+    $('.game-dev').removeClass('three columns');
+    $('.edu-program').removeClass('six columns');
+    $('.employer').removeClass('six columns');
+    $('.project').removeClass('six columns');
+  }
   $(window).on('resize', function() {
-    var win= $(this);
-    if (win.width() > 750) {
-      $('.programming-languages').addClass('three columns');
-      $('.web-dev').addClass('six columns');
-      $('.game-dev').addClass('three columns');
-      $('.edu-program').addClass('six columns');
-      $('.employer').addClass('six columns');
-      $('.project').addClass('six columns');
-    } else {
-      $('.programming-languages').removeClass('three columns');
-      $('.web-dev').removeClass('six columns');
-      $('.game-dev').removeClass('three columns');
-      $('.edu-program').removeClass('six columns');
-      $('.employer').removeClass('six columns');
-      $('.project').removeClass('six columns');
-    }
   });
 });
